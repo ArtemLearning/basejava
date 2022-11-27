c568112fb97b6710be90acba11e75da97cd20ca7
ArtemLearning <ArtemLearning@yandex.ru>
2022-11-27 23:48:15 +0300

    27.11.2022 Basejava start. Initial lesson.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,52 @@
-/**
- * Array based storage for Resumes
- */
+import java.util.Arrays;
+
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int size;
 
     void clear() {
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+        storage[size] = r;
+        size++;
     }
 
     Resume get(String uuid) {
-        return null;
+        for (int i = 0; i < size; i++) {
+            Resume resume = storage[i];
+            if (resume.uuid.equals(uuid)) {
+                return resume;
+            }
+        }
+        Resume resume = new Resume();
+        resume.uuid = "dummy";
+        return resume;
     }
 
     void delete(String uuid) {
+        int position = -1;
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                position = i;
+            }
+            if (position != -1) {
+                System.arraycopy(storage, position + 1, storage, position, size - (position + 1));
+                size--;
+            } else {
+                System.out.println("Нет резюме с таким uuid");
+            }
+        }
     }
 
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
