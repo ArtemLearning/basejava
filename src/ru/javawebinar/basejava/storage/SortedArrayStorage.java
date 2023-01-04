c0c8277df5dbaa641e81cfe7703cb2ff50823694
ArtemLearning <ArtemLearning@yandex.ru>
2023-01-04 12:50:04 +0300

    04.01.2023 Fixed SortedArrayStorage methods.

--- src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
@@ -8,16 +8,35 @@ public class SortedArrayStorage extends AbstractArrayStorage {
 
     @Override
     public void save(Resume r) {
-        Arrays.sort(storage, 0, size);
+        if (size == STORAGE_LIMIT) {
+            throw (new RuntimeException("База резюме полностью заполнена"));
+        } else if (getIndex(r.getUuid()) > 0) {
+            throw (new RuntimeException("Резюме c uuid " + r.getUuid() + " уже есть в базе"));
+        } else {
+            for (int i = 0; i < size; i++) {
+                if (storage[i].getUuid().compareTo(r.getUuid()) > 0) {
+                    for (int j = size; j > i; j--) {
+                        storage[j] = storage[j - 1];
+                    }
+                    storage[i] = r;
+                    size++;
+                    return;
+                }
+            }
+            storage[size] = r;
+            size++;
+        }
     }
 
     @Override
     public void delete(String uuid) {
         int index = getIndex(uuid);
         if (index != -1) {
-            storage[index] = storage[size - 1];
             storage[size - 1] = null;
             size--;
+            for (int j = index; j < size; j++) {
+                storage[j] = storage[j + 1];
+            }
         } else {
             throw (new RuntimeException("Нет резюме с uuid " + uuid));
         }
