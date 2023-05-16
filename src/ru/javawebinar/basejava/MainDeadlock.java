b6731313e1a7e1864641499d43fc30ee4385d77f
ArtemLearning <ArtemLearning@yandex.ru>
2023-05-16 09:51:21 +0300

    15.05.2023 basejava lesson 11 HW deadlock by methods

--- src/ru/javawebinar/basejava/MainDeadlock.java
+++ src/ru/javawebinar/basejava/MainDeadlock.java
@@ -1,39 +1,39 @@
 package ru.javawebinar.basejava;
 
 public class MainDeadlock {
+    final String str1 = "Object1";
+    final String str2 = "Object2";
 
-    public static void main(String[] args) {
-        Object obj1 = new Object();
-        Object obj2 = new Object();
-
-        Thread thread1 = new Thread(() -> {
-            try {
-                synchronized (obj1) {
-                    System.out.println("Thread1 - Object1");
-                    Thread.sleep(100);
-                    synchronized (obj2) {
-                        System.out.println("Thread1 - Object2");
-                    }
-                }
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        });
-        Thread thread2 = new Thread(() -> {
-            try {
-                synchronized (obj2) {
-                    System.out.println("Thread2 - Object2");
-                    Thread.sleep(100);
-                    synchronized (obj1) {
-                        System.out.println("Thread2 - Object1");
-                    }
-                }
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        });
+    public static void main(String[] args) throws InterruptedException {
+        MainDeadlock deadlock = new MainDeadlock();
+        Thread thread1 = deadlock.createThread(deadlock.str1, deadlock.str2);
+        Thread thread2 = deadlock.createThread(deadlock.str2, deadlock.str1);
         System.out.println("Start Deadlock");
         thread1.start();
         thread2.start();
+    }
+
+    private Thread createThread(String str1, String str2) {
+        return new Thread(() -> {
+            try {
+                lockObject(str1, str2);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+    private void lockObject(String str1, String str2) throws InterruptedException {
+        try {
+            synchronized (str1) {
+                System.out.println(Thread.currentThread() + " - " + str1);
+                Thread.sleep(100);
+                synchronized (str2) {
+                    System.out.println(Thread.currentThread() + " - " + str2);
+                }
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
