08236c504381acb570b8a7edb87a61ac9d069a1e
ArtemLearning <ArtemLearning@yandex.ru>
2023-05-18 16:44:53 +0300

    18.05.2023 basejava lesson 12 HW done fixed

--- src/ru/javawebinar/basejava/MainStream.java
+++ src/ru/javawebinar/basejava/MainStream.java
@@ -6,34 +6,29 @@ import java.util.stream.Collectors;
 
 public class MainStream {
 
-    public MainStream() {
-    }
-
     public static void main(String[] args) {
-        MainStream mainStream = new MainStream();
-        System.out.println(mainStream.minValue(new int[]{1, 2, 3, 3, 2, 3}));
+        System.out.println(minValue(new int[]{1, 2, 3, 3, 2, 3}));
 
-        List<Integer> list = mainStream.oddOrEven(List.of(new Integer[]{1, 4, 5, 6, 7}));
+        List<Integer> list = oddOrEven(List.of(new Integer[]{1, 4, 5, 6, 7}));
         for (Integer i : list) {
             System.out.println(i.toString());
         }
 
     }
 
-    public int minValue(int[] values) {
-        final int[] result = new int[1]; // New int array of 1 element
-        Arrays.stream(values)
-                .boxed()                 // Convert to boxed Integer
+    private static int minValue(int[] values) {
+        return Arrays.stream(values)
                 .distinct()              // Get distinct numbers
                 .sorted()                // Sort distinct number ascended
-                .mapToInt(Integer::intValue).forEach(i -> result[0] = i + result[0] * 10);  // get number and put in into result array
-        return result[0];
+                .reduce((a, b) -> a * 10 + b)
+                .orElseThrow();
     }
 
-    public List<Integer> oddOrEven(List<Integer> integers) {
+    private static List<Integer> oddOrEven(List<Integer> integers) {
+        int sum = integers.stream().reduce(0, Integer::sum);
         return integers.stream()
-                .filter(x -> (x % 2) == (integers.stream().reduce(0, Integer::sum) % 2)) // Get sum of elements and check if it odd or even, and filter stream elements
-                .distinct()                                                              // Get distinct numbers
-                .collect(Collectors.toList());                                           // Convert to list
+                .filter(x -> (x % 2) == (sum % 2))
+                .distinct()
+                .collect(Collectors.toList());
     }
 }
