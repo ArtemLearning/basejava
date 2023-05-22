780a520b4ed54124181bb1cd80d4f25e0b2de5ab
ArtemLearning <ArtemLearning@yandex.ru>
2023-05-22 13:03:03 +0300

    21.05.2023 basejava lesson 13 HW done all

--- src/ru/javawebinar/basejava/util/SqlHelper.java
+++ src/ru/javawebinar/basejava/util/SqlHelper.java
@@ -33,32 +33,4 @@ public class SqlHelper {
             throw new StorageException(e);
         }
     }
-
-    public Connection getConnection() {
-        return connection;
-    }
-    //    public void doRequest(String request, String str1, String str2) throws SQLException {
-//        prepare(request, str1, str2);
-//        preparedStatement.execute();
-//    }
-//
-//    public ResultSet doRequestQuery(String request, String str1, String str2) throws SQLException {
-//        prepare(request, str1, str2);
-//        return preparedStatement.executeQuery();
-//    }
-//
-//    public int doRequestUpdate(String request, String str1, String str2) throws SQLException {
-//        prepare(request, str1, str2);
-//        return preparedStatement.executeUpdate();
-//    }
-//
-//    private void prepare(String request, String str1, String str2) throws SQLException {
-//        preparedStatement = connection.prepareStatement(request);
-//        if (str1 != null) {
-//            preparedStatement.setString(1, str1);
-//        }
-//        if (str2 != null) {
-//            preparedStatement.setString(2, str2);
-//        }
-//    }
 }
