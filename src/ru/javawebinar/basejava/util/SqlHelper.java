388b0dea3fe01c699aee823430d09b4b531af298
ArtemLearning <ArtemLearning@yandex.ru>
2023-05-22 13:02:01 +0300

    21.05.2023 basejava lesson 13 HW done all

--- src/ru/javawebinar/basejava/util/SqlHelper.java
+++ src/ru/javawebinar/basejava/util/SqlHelper.java
@@ -1,45 +1,64 @@
 package ru.javawebinar.basejava.util;
 
+import ru.javawebinar.basejava.exception.StorageException;
 import ru.javawebinar.basejava.sql.ConnectionFactory;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class SqlHelper {
     public final Connection connection;
-    private PreparedStatement preparedStatement;
 
-//    public interface Request{
-//        AnEntity execute(EntityManager em);
-//    }
+    @FunctionalInterface
+    public interface Request<T> {
+        T execute(PreparedStatement ps) throws SQLException;
+    }
+
     public SqlHelper(ConnectionFactory connectionFactory) throws SQLException {
         connection = connectionFactory.getConnection();
     }
 
-    public void doRequest(String request, String str1, String str2) throws SQLException {
-        prepare(request, str1, str2);
-        preparedStatement.execute();
-    }
-
-    public ResultSet doRequestQuery(String request, String str1, String str2) throws SQLException {
-        prepare(request, str1, str2);
-        return preparedStatement.executeQuery();
-    }
-
-    public int doRequestUpdate(String request, String str1, String str2) throws SQLException {
-        prepare(request, str1, str2);
-        return preparedStatement.executeUpdate();
-    }
-
-    private void prepare(String request, String str1, String str2) throws SQLException {
-        preparedStatement = connection.prepareStatement(request);
-        if (str1 != null) {
-            preparedStatement.setString(1, str1);
-        }
-        if (str2 != null) {
-            preparedStatement.setString(2, str2);
+    public <T> T runRequest(String statement, String str1, String str2, Request<T> t) {
+        try {
+            PreparedStatement ps = connection.prepareStatement(statement);
+            if (str1 != null) {
+                ps.setString(1, str1);
+            }
+            if (str2 != null) {
+                ps.setString(2, str2);
+            }
+            return t.execute(ps);
+        } catch (SQLException e) {
+            throw new StorageException(e);
         }
     }
+
+    public Connection getConnection() {
+        return connection;
+    }
+    //    public void doRequest(String request, String str1, String str2) throws SQLException {
+//        prepare(request, str1, str2);
+//        preparedStatement.execute();
+//    }
+//
+//    public ResultSet doRequestQuery(String request, String str1, String str2) throws SQLException {
+//        prepare(request, str1, str2);
+//        return preparedStatement.executeQuery();
+//    }
+//
+//    public int doRequestUpdate(String request, String str1, String str2) throws SQLException {
+//        prepare(request, str1, str2);
+//        return preparedStatement.executeUpdate();
+//    }
+//
+//    private void prepare(String request, String str1, String str2) throws SQLException {
+//        preparedStatement = connection.prepareStatement(request);
+//        if (str1 != null) {
+//            preparedStatement.setString(1, str1);
+//        }
+//        if (str2 != null) {
+//            preparedStatement.setString(2, str2);
+//        }
+//    }
 }
